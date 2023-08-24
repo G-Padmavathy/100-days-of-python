124694392352fd14d9218e2889134b1e523ec507
Padmavathy G <96161598+G-Padmavathy@users.noreply.github.com>
2023-08-24 09:42:32 +0530

    Add files via upload

---
+++ Day-1/Exercise 1-Printing.py
@@ -0,0 +1,8 @@
+""""sample output
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')"""
+
+# Write your code below this line 👇
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
