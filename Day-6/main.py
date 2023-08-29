17fbc6f14ab07a6a5ccf1ed77cb7fcbc3940ef4d
Padmavathy G <96161598+G-Padmavathy@users.noreply.github.com>
2023-08-29 18:22:20 +0530

    Add files via upload

---
+++ Day-6/main.py
@@ -0,0 +1,47 @@
+"""
+Function:A function in Python is a reusable block of code that performs a specific task.
+It allows you to group related code together and execute it by calling the function's name.
+Functions help in organizing your code, making it more readable, and enabling you to avoid code duplication.
+def is a keyword
+function name with () parentheses:
+
+"""
+# Example
+# defining a function
+def my_function():
+    print("first function")
+
+# calling a function
+my_function()
+
+"""
+Indention:
+
+Example:
+def function():
+....print("Hello")
+....print("padma")
+Print("Have a Nice Day")
+
+Example:
+def funtion(): # start function block
+....if sky == "clear": # start if block
+........print("Blue") # end if block
+....elif sky == "Cloudy":
+........print("Grey")
+....print("hello") # end funtion block
+print("World")
+
+"""
+
+"""
+While Loops:A while loop in Python is used to repeatedly execute a block of code as long as a certain condition is True. 
+Syntax:
+while condition: is True
+    DO SOMTHETHIG UNTIL IT BECOME FALSE
+"""
+
+x = 6
+while x > 0:
+    print(x)
+    x -= 1
